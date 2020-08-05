a406cda23563c0b1457dd333917e768803320e65
Niels Mündler <n.muendler@tum.de>
2020-08-05 16:12:33 +0200

    Improve perturbation algorithms

--- perturb.py
+++ perturb.py
@@ -1,14 +1,26 @@
-from sympy import Matrix, Rational, symbols
-from simplex import is_generic
+from typing import *
+from sympy import Matrix, Rational, symbols, RealNumber
+from verification import *
 
 
-def make_generic_explicit_perturb(A: Matrix, b: Matrix, force=False):
-    m, n = A.shape
-    if not force and is_generic(A, b):
-        return A, b, m*[0]
-    eps = symbols("\epsilon")
-    eps_sub = Rational(1, 64)
-    e = Matrix([eps**(i+1) for i in range(m)])
-    while not is_generic(A, b+(e.subs(eps, eps_sub))):
-        eps_sub /= 2
-    return A, b + (e.subs(eps, eps_sub)), e.subs(eps, eps_sub)
+def pertubation_vector(perm: Iterable[int], eps):
+    """
+    Provided a sufficiently small epsilon returns a pertubation vector
+    s.t. (A, b+e(\epsilon)) is in general position
+    :param: permutation of m constraints, usually `range(m)`
+    :param eps:
+    :return: e(\epsilon)
+    """
+    return Matrix([eps**(i+1) for i in perm])
+
+
+def perturbed_polygon(A: Matrix, b: Matrix, eps=Rational(1, 128)):
+    m = A.shape[0]
+    return A, b+pertubation_vector(range(m), eps)
+
+
+def v_star_from_perturbed_polygon(A: Matrix, b: Matrix, A_pert: Matrix, b_pert: Matrix, v_pert: Matrix):
+    I_pert = active_constraints(v_pert, A_pert, b_pert)
+    v_star = (sub_matrix(A, I_pert)**-1)*sub_matrix(b, I_pert)
+    return v_star
+
